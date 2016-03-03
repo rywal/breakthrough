375c102d4d2ceef767d4771f8595d2880dc1bead
Jonathan Grimes <jonygrimes@tamu.edu>
2016-03-02 20:51:48 -0600

    Modifing old parser this is to be as effiecent as possible

--- Game/Parser.cpp
+++ Game/Parser.cpp
@@ -1,26 +1,81 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <fstream>
+
+//------GLOBAL-VARIABLE------//
+ofstream output;
+int error;
+int line_number;
+string delimiters;
+size_t buffer_size;
+char* str;
+char* pch;
+//---------------------------//
+
+void clear_input(){ //  This
+	cin.clear();   //   Ensures
+	cin.sync();   //    cin is
+	cin.ignore();//     cleared (For sure)
+}
+
+int file_input(FILE *input, string filename, bool is_open){
+	//--------Local-Variables--------//
+	vector<string> command_list;
+	//-------------------------------//
+	
+	while(!feof(input)){ //get a command from the file
+		getline(&str, &buffer_size, input); 
+		string command(str);
+		printf("The given command is: %s\n",str); //prints the command back to the user
+		pch = strtok (str, delimiters.c_str()); //tokenizes the command
+		while (pch != NULL) {
+			command_list.push_back(pch); //put the token into a vector to make the command easy to parse
+			pch = strtok (NULL, delimiters.c_str());
+		}
+		if(command_list[0]=="EXIT" && command_list.size()==1){//Checking for "EXIT" command - Preventing SegFault
+			output<<"-=-=-=-=-EXITED-=-=-=-=-"<<endl;
+			exit(0);
+		} else{	
+		//This marks the beginning of the query/command interpretation
+			//query_or_command(db, command_list);   //////////////////////////////////////////////////////////////////
+		}
+		if(error==0){
+			output<<"Line number "<<line_number<<" was successful!"<<endl; //Prints status to Output file
+		} else {
+			output<<"Line number "<<line_number<<" FAILED!"<<endl; //Prints status to Output file
+		}
+		//prepare for next command
+		command_list.clear();
+		free(pch);
+		line_number++;
+		error=0;
+	}
+	fclose(input);
+}
+
 int main() {
 	output.open ("Output.txt");
 	output<<"-=-=-=-=-BEGIN=-=-=-=-=-"<<endl;
-	Database db("db");
-	char* str;
-	char* pch;
+	//------Definining-Variables------//
 	int i=0;
-	size_t buffer_size=0;
-	string delimiters = " ,();\n";
-	vector<string> command_list;	
-	if(!output){ //This should never happen
-		printf("\nThe Output file is not found!");
-		exit(EXIT_FAILURE);//Showing error status code
-	}
+	buffer_size=0;
+	delimiters = " ,();\n";
+	vector<string> command_list;
+	line_number=1;
+	//--------------------------------//
+	
 	string f_or_h;
-	printf("For this run, would you like to take input from an \"Input.txt\" file [f], or type commands in by hand[h]?\n(If your input has a \".txt\" in it, it'll be processes as a file)\n>");//Giving better testing handles
+	printf("Intake from file [f] or hand[h]?\n(\".txt\"'s in input will be assumed as a file)\n>");//Giving better testing handles
 	cin >>  f_or_h;
+	clear_input();//Clears CIN
+	if(f_or_h == ""){f_or_h="h";} //Assume that you want to write by hand
 	int loop=1;
 	while (loop==1){
-		if(f_or_h != "h" && f_or_h != "H" && f_or_h != "hand" && f_or_h != "f" && f_or_h != "F" && f_or_h != "file" && !strstr(f_or_h.c_str(),".txt")){
-			cin.clear();//  This
-			cin.sync();//   Ensures
-			cin.ignore();// Everything is cleared (For sure)
+		if(f_or_h != "h" && f_or_h != "H" && f_or_h != "f" && f_or_h != "F" && !strstr(f_or_h.c_str(),".txt")){
+			clear_input();//Clears CIN
 			printf("Please re-enter your prefered input method [f\\h]\n>");
 			cin >>  f_or_h;
 		} else{loop=0;}
@@ -35,21 +90,22 @@ int main() {
 			filename = "Game/" + f_or_h;
 			input=fopen(input_w.c_str(), "r");
 		}else{
-			printf("Please input the file you would like to use. \n(Please note, this is automated, no other input will be read)\n>");
-			cin.clear();//  This
-			cin.sync();//   Ensures
-			cin.ignore();// Everything is cleared (For sure)
-			cin>>input_f;
-			string input_w = "Game/" + input_f;
-			filename = "Game/" + input_f;
-			input=fopen(input_w.c_str(), "r");
+			loop=1;
+			while (loop==1){ //Ask for input multiple times
+				printf("Please input the file you would like to use. \n(Please note, this is automated, no other input will be read)\n>");
+				cin>>input_f;
+				string input_w = "Game/" + input_f;
+				filename = "Game/" + input_f;
+				input=fopen(input_w.c_str(), "r");
+				if(!input){
+					printf("\nThe file: %s not found!", filename.c_str());
+					printf("\nPlease place the input folder as in the same folder as %s.\n\n", __FILE__);
+				}else{loop=0;}
+			}
 		}
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
 		//file_input(db, input, filename, false);//Where the action happens
-	} else if (f_or_h == "h" || f_or_h == "H" || f_or_h == "hand"){
-		cin.clear();
-		cin.sync();
-		cin.ignore();
-		int command_number=1;
+	} else if (f_or_h == "h" || f_or_h == "H"){
 		while(1){
 			error=0;
 			printf("Please enter a command:\n");
@@ -63,17 +119,20 @@ int main() {
 			}
 			if(command_list[0]=="EXIT" && command_list.size()==1){//Preventing SegFault
 				exit(0);
-			} else{query_or_command(db, command_list);}
-			if(error==0){
-				output<<"Command number "<<command_number<<" was successful!"<<endl;
 			} else{
-				output<<"Command number "<<command_number<<" Failed!"<<endl;
+				//query_or_command(db, command_list); ////////////////////////////////////////////////////////////////
+				}
+			if(error==0){
+				output<<"Command number "<<line_number<<" was successful!"<<endl;
+			} else{
+				output<<"Command number "<<line_number<<" Failed!"<<endl;
 			}
 			command_list.clear();
 			free(pch);
-			command_number++;
+			line_number++;
 		}
 	}
+	output<<"-=-=-=-=-=END-=-=-=-=-=-";
 	output.close();
     return 0;
 }
