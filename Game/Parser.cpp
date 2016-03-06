357448e9058f1335c2f8a4b247a70f083c2607d8
Jonathan Grimes <jonygrimes@tamu.edu>
2016-03-06 12:30:30 -0600

    Fixed spacing in Parser.cpp

--- Game/Parser.cpp
+++ Game/Parser.cpp
@@ -2,76 +2,39 @@
 
 
 int main() {
-
 	//------INCLUDE-WITH-ALL-MAINS-USED------//
-
 	output.open ("Output.txt");
-
 	output<<"=-=-=-=-=-=-BEGIN=-=-=-=-=-=-="<<endl;
-
 	buffer_size=0;
-
 	delimiters = " \n";
-
 	line_number=1;
-
 	signal(SIGINT, ctrl_c); //Catch Ctrl+C (For output format)
-
 	signal(SIGSEGV, ctrl_c);//Catch SegFaults (For output format)
-
 	//------^-^-^-^-^-^-^-^-^-^-^-^-^-^-^------//
-
 	
 	//Example of a main-----------------------------------------------------//
-
 	while(true){
-
 		while(1){
-
 		printf("file[f] or hand[h]?\n>");//Giving better testing handles
-
 		cin >>  f_or_h;
-
 		clear_input();//Clears CIN
-
 		int loop=1;
-
 		while (loop==1){
-
 				if(f_or_h != "h" && f_or_h != "H" && f_or_h != "f" && f_or_h != "F"){
-
 					printf("Please re-enter your prefered input method [f\\h]\n>");//TEST MAIN
-
 					cin >>  f_or_h;
-
 				} else{loop=0;}
-
 			}
-
 			if(f_or_h == "f" || f_or_h == "F"){
-
 				parse_file(line_number);
-
 			} else if (f_or_h == "h" || f_or_h == "H"){
-
 				while(1){
-
 					take_command(line_number);
-
 				}
-
 			}
-
 		}
-
 	}
-
 	output<<"=-=-=-=-=-=-EXITED-=-=-=-=-=-=";
-
 	output.close();
-
     return 0;
-
-
-
 }
