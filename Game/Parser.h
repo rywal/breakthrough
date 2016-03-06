a226565698ada43d8a118ecf64275e35d20ea592
Jonathan Grimes <jonygrimes@tamu.edu>
2016-03-05 20:47:47 -0600

    Fixed merge conflict

--- Game/Parser.h
+++ Game/Parser.h
@@ -245,7 +245,7 @@ void do_command(vector<string> command_line){
 >>>>>>> 93d8455... Fixed merge conflict
 							new_game.update(tc, t2-48, to_dir(command_line[1].c_str()));
 						} else{
-							printf("%c%c is an invalid move\n", tc,t2);
+							printf("%c%c %s is an invalid move\n", tc,t2, comman_line[1]);
 							output<<tc<<t2<<" is an invalid move"<<endl;
 							error = 16;
 						}
