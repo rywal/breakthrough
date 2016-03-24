613ef1186a0b95380427e8bf51e5a5bd66d4d1e9
Ryan Walters <ryan@ryanwalters.me>
2016-03-23 22:23:19 -0500

    fixed column check

--- Game/AI.cpp
+++ Game/AI.cpp
@@ -211,7 +211,7 @@ void save_children(vector<vector<char>> node_board, vector<Node*> &children_node
 		int row = white ? i+1 : i-1;
 		int col = white ? j+count : j-count; //Direction will be the same for both turns
 		if((row < 9) && (row >= 0)){
-            if (col < 7 && col >= 0) {
+            if (col <= 7 && col >= 0) {
                 if(node_board[row][col]!=turn){
                     //--Change the new board
                     node_board[i][j]='_';
