71307f03c096407541481463625337768edd2a26
Ryan Walters <ryan@ryanwalters.me>
2016-03-23 22:22:52 -0500

    fixed row check to include row 8

--- Game/AI.cpp
+++ Game/AI.cpp
@@ -210,18 +210,20 @@ void save_children(vector<vector<char>> node_board, vector<Node*> &children_node
 		char turn = white ? 'o' : 'x';
 		int row = white ? i+1 : i-1;
 		int col = white ? j+count : j-count; //Direction will be the same for both turns
-		if((i<7)&&(i>0)){
-			if(node_board[row][col]!=turn){
-				//--Change the new board
-				node_board[i][j]='_';
-				node_board[row][col]=turn;
-				//--Create the new Node
-				Node* new_node = new Node(value_node(node_board), parent, i, j, count);
-				if(new_node->get_depth()<max_depth){
-					new_node->set_children(get_children(new_node, node_board, !white, max_depth, depth_list));
-				}//--Push back the whole Node
-				children_nodes.push_back(new_node);
-			}
+		if((row < 9) && (row >= 0)){
+            if (col < 7 && col >= 0) {
+                if(node_board[row][col]!=turn){
+                    //--Change the new board
+                    node_board[i][j]='_';
+                    node_board[row][col]=turn;
+                    //--Create the new Node
+                    Node* new_node = new Node(value_node(node_board), parent, i, j, count);
+                    if(new_node->get_depth()<max_depth){
+                        new_node->set_children(get_children(new_node, node_board, !white, max_depth, depth_list));
+                    }//--Push back the whole Node
+                    children_nodes.push_back(new_node);
+                }
+            }
 		}
 	}
 }
@@ -239,7 +241,7 @@ vector<Node*> get_children(Node* parent, vector<vector<char>> parent_board, bool
                             continue;
                         }
 						save_children(parent_board, children_nodes, parent, white ,i,j,e, max_depth, depth_list);
-					}else if(!white && (!(j ==7 && e == -1) && !(j == 0 && e == 1))){ //For formating
+					}else if(!white && (!(j == 7 && e == -1) && !(j == 0 && e == 1))){ //For formating
                         if (i > 0 && parent_board[i-1][j] == 'o' && e == 0) {
                             continue;
                         }
