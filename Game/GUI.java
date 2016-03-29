d0a5b3971ba35fbe731e31c292559409601fc9f5
Jonathan Grimes <jonygrimes@tamu.edu>
2016-03-28 21:16:47 -0500

    Updated color Scheme Stil needs a color change, but core is there

--- Game/GUI.java
+++ Game/GUI.java
@@ -15,6 +15,12 @@ class GUI{
 	
 	static JButton [][] buttons = new JButton[8][8]; 
 	static JTextArea outputField;
+	static ImageIcon wgIcon = new ImageIcon("Game/wg.png");//White piece on green background
+	static ImageIcon wmIcon = new ImageIcon("Game/wm.png");//White piece on maroon background
+	static ImageIcon bgIcon = new ImageIcon("Game/bg.png");//Black piece on green background
+	static ImageIcon bmIcon = new ImageIcon("Game/bm.png");//Black piece on maroon background
+	static ImageIcon egIcon = new ImageIcon("Game/eg.png");//Empty piece on green background
+	static ImageIcon emIcon = new ImageIcon("Game/em.png");//Empty piece on maroon background
 	
 	public static JPanel fullPanel(JPanel topPanel, JPanel centerPanel) {
 		JPanel full = new JPanel(new BorderLayout());
@@ -39,17 +45,13 @@ class GUI{
 		top.add(startOver);
 		
 		JPanel bottom = new JPanel(/*new GridLayout(2, 1)*/);
-		String[] gameTypeString = {"human-ai", "ai-ai"};
-		String[] aiString1 = {"Easy", "Medium", "Hard"};
-		String[] aiString2 = {"Easy", "Medium", "Hard"};
+		String[] gameTypeString = {"Game Type", "human-ai", "ai-ai"};
+		String[] aiString1 = {"Difficulty #1", "Easy", "Medium", "Hard"};
+		String[] aiString2 = {"Difficulty #2", "Easy", "Medium", "Hard"};
 		
 		JComboBox gameType = new JComboBox(gameTypeString);
 		JComboBox aiDif1 = new JComboBox(aiString1);
 		JComboBox aiDif2 = new JComboBox(aiString2);
-		
-		gameType.setEditable(true);
-		aiDif1.setEditable(true);
-		aiDif2.setEditable(true);
 		
 		//gameType.addActionListener(this);
 		bottom.add(gameType);
@@ -62,32 +64,49 @@ class GUI{
 		return fullTop;
 	}
 	
+	public static JButton defaultLayout(int row, int col, int white){//0=black, 1=white, 2=empty
+		if(white==0 && (((row%2==0) && (col%2==0)) || ((row%2!=0) && (col%2!=0)))){
+			buttons[row][col] = new JButton(bgIcon);
+		} else if(white==0 && (((row%2!=0) && (col%2==0)) || ((row%2==0) && (col%2!=0)))){
+			buttons[row][col] = new JButton(bmIcon);
+		} else if(white==1 && (((row%2==0) && (col%2==0)) || ((row%2!=0) && (col%2!=0)))){
+			buttons[row][col] = new JButton(wgIcon);
+		} else if(white==1 && (((row%2!=0) && (col%2==0)) || ((row%2==0) && (col%2!=0)))){
+			buttons[row][col] = new JButton(wmIcon);
+		} else if(white==2 && (((row%2==0) && (col%2==0)) || ((row%2!=0) && (col%2!=0)))){
+			buttons[row][col] = new JButton(egIcon);
+		} else if(white==2 && (((row%2!=0) && (col%2==0)) || ((row%2==0) && (col%2!=0)))){
+			buttons[row][col] = new JButton(emIcon);
+		} else{
+			buttons[row][col] = new JButton(emIcon); //Just in case
+		}
+		
+		return buttons[row][col];
+	}
+	
 	public static JPanel centerPanel() {
 		JPanel center = new JPanel(new GridLayout(8, 8));
-		ImageIcon xIcon = new ImageIcon("Game/x.png");
-		ImageIcon oIcon = new ImageIcon("Game/o.png");
-		ImageIcon emptyIcon = new ImageIcon("Game/empty.png");
 
 		//Create the 2D array by using two for loops
 		for(int row = 0; row < 2; row++) {
-			for(int col = 0; col < 8; col++) { 
-				buttons[row][col] = new JButton(xIcon);
-				buttons[row][col].setPreferredSize(new Dimension(1,1));
-				center.add(buttons[row][col]);
+			for(int col = 0; col < 8; col++) {
+				//buttons[row][col] = new JButton(xIcon);
+				//buttons[row][col].setPreferredSize();
+				
+				center.add(defaultLayout(row, col, 0));
 			}
 		}
 		for(int row = 2; row < 6; row++) {
 			for(int col = 0; col < 8; col++) { 
-				buttons[row][col] = new JButton(emptyIcon);
-				center.add(buttons[row][col]);
+				center.add(defaultLayout(row, col, 2));
 			}
 		}
 		for(int row = 6; row < 8; row++) {
 			for(int col = 0; col < 8; col++) { 
-				buttons[row][col] = new JButton(oIcon);
-				center.add(buttons[row][col]);
+				center.add(defaultLayout(row, col, 1));
 			}
 		}
+		
 		/*
 		//Create the 2D array by using state
 		for(int row = 0; row < 8; row++) {
@@ -106,6 +125,7 @@ class GUI{
 		center.setPreferredSize(new Dimension(450, 450));
 		return center;
 		*/
+		
 		center.setPreferredSize(new Dimension(450, 450));
 		return center;
 	}
